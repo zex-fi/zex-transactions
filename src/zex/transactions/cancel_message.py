8a35e40e6f2cb539b28a634ad5f7baa351c6cc64
Sadegh Moayedizadeh <sadegh.moayedizadeh@gmail.com>
2026-06-16 11:50:24 +0330

    Fix a message.

--- src/zex/transactions/cancel_message.py
+++ src/zex/transactions/cancel_message.py
@@ -80,7 +80,7 @@ class CancelMessage(BaseMessage):
     @property
     def key_identifier(self) -> int:
         if self._key_identifier is None:
-            raise AttributeError("key_identifier is not available in v1/v2 messages.")
+            raise AttributeError("key_identifier is not available in v2 messages.")
         return self._key_identifier
 
     @classmethod
